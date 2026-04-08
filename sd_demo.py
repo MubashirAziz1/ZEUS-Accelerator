0f63fa37f92926b403cc7f21c2a66aa1f5b38692
MubashirAziz1 <141399769+MubashirAziz1@users.noreply.github.com>
2026-04-08 06:31:55 +0500

    Update sd_demo.py

--- sd_demo.py
+++ sd_demo.py
@@ -128,60 +128,60 @@ if __name__ == "__main__":
     del baseline_pipe
     torch.cuda.empty_cache()
 
-    # Zeus
-    pipe = StableDiffusionPipeline.from_pretrained(args.model, torch_dtype=torch.float16).to("cuda:0")
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    if args.solver == "dpm":
-        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    if args.solver == "euler":
-        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
-    # pipe = load_lora_weights(pipe, lora_path)
+    # # Zeus
+    # pipe = StableDiffusionPipeline.from_pretrained(args.model, torch_dtype=torch.float16).to("cuda:0")
+    # pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    # if args.solver == "dpm":
+    #     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    # if args.solver == "euler":
+    #     pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    # # pipe = load_lora_weights(pipe, lora_path)
 
-    patch.apply_patch(pipe,
-                      acc_range=(10, 45),
+    # patch.apply_patch(pipe,
+    #                   acc_range=(10, 45),
 
-                      interp_mode="psi",
-                      caching_mode="reuse_interp",
-                      denominator=3,
-                      modular=(0, 1),
+    #                   interp_mode="psi",
+    #                   caching_mode="reuse_interp",
+    #                   denominator=3,
+    #                   modular=(0, 1),
 
-                      lagrange_int=4,
-                      lagrange_step=24,
-                      lagrange_term=4,
+    #                   lagrange_int=4,
+    #                   lagrange_step=24,
+    #                   lagrange_term=4,
 
-                      max_interval=6)
+    #                   max_interval=6)
 
-    logging.info("Warming up GPU...")
-    for _ in range(2):
-        set_random_seed(seed)
-        _ = pipe(prompt, output_type='pt', height=args.height, width=args.width, num_inference_steps=50).images
-        patch.reset_cache(pipe)
+    # logging.info("Warming up GPU...")
+    # for _ in range(2):
+    #     set_random_seed(seed)
+    #     _ = pipe(prompt, output_type='pt', height=args.height, width=args.width, num_inference_steps=50).images
+    #     patch.reset_cache(pipe)
 
-    logging.info("Running Cache-Assited Pruning...")
-    set_random_seed(seed)
+    # logging.info("Running Cache-Assited Pruning...")
+    # set_random_seed(seed)
 
-    start_time = time.time()
-    cap_output = pipe(prompt, output_type='pt', height=args.height, width=args.width, num_inference_steps=50).images
-    use_time = time.time() - start_time
+    # start_time = time.time()
+    # cap_output = pipe(prompt, output_type='pt', height=args.height, width=args.width, num_inference_steps=50).images
+    # use_time = time.time() - start_time
 
-    print(pipe.unet._cache_bus.rel_momentum_list)
-    print(pipe.unet._cache_bus.abs_momentum_list)
-    print(pipe.unet._cache_bus.skipping_path)
+    # print(pipe.unet._cache_bus.rel_momentum_list)
+    # print(pipe.unet._cache_bus.abs_momentum_list)
+    # print(pipe.unet._cache_bus.skipping_path)
 
-    logging.info("⚡Zeus: {:.2f} seconds".format(use_time))
+    # logging.info("⚡Zeus: {:.2f} seconds".format(use_time))
 
-    save_image([ori_output[0], cap_output[0]], "output.png")
+    save_image(ori_output[0], "/kaggle/working/output.png")
     logging.info("Saved to output.png. Done!")
 
-    print("Evaluating LPIPS")
-    p_r = torch.stack([T.Compose([
-        T.Normalize((0.5,), (0.5,))
-    ])(img) for img in ori_output]).to('cuda')
+    # print("Evaluating LPIPS")
+    # p_r = torch.stack([T.Compose([
+    #     T.Normalize((0.5,), (0.5,))
+    # ])(img) for img in ori_output]).to('cuda')
 
-    p_o = torch.stack([T.Compose([
-        T.Normalize((0.5,), (0.5,))
-    ])(img) for img in cap_output]).to('cuda')
+    # p_o = torch.stack([T.Compose([
+    #     T.Normalize((0.5,), (0.5,))
+    # ])(img) for img in cap_output]).to('cuda')
 
-    loss_fn_alex = lpips.LPIPS(net='alex').to('cuda')
-    d = loss_fn_alex(p_r, p_o)
-    print(f"LPIPS: {d.item()}")
+    # loss_fn_alex = lpips.LPIPS(net='alex').to('cuda')
+    # d = loss_fn_alex(p_r, p_o)
+    # print(f"LPIPS: {d.item()}")
