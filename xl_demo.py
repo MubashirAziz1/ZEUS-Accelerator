1920ca0377fa6c4d24c81f9dde9ebc15e7df5469
MubashirAziz1 <141399769+MubashirAziz1@users.noreply.github.com>
2026-04-08 06:49:15 +0500

    Refactor pipeline setup for baseline and Zeus models
    Refactor the pipeline setup for baseline and Zeus models, including GPU warmup and logging.

--- xl_demo.py
+++ xl_demo.py
@@ -32,72 +32,72 @@ if __name__ == "__main__":
     seed = args.seed
     prompt = args.prompt
 
-    baseline_pipe = StableDiffusionXLPipeline.from_pretrained(
-        args.model, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-    ).to("cuda:0")
-    if args.solver == "dpm":
-        baseline_pipe.scheduler = DPMSolverMultistepScheduler.from_config(baseline_pipe.scheduler.config)
-    if args.solver == "euler":
-        baseline_pipe.scheduler = EulerDiscreteScheduler.from_config(baseline_pipe.scheduler.config)
-    # baseline_pipe.load_lora_weights(lora_path)
-
-    # Warmup GPU. Only for testing the speed.
-    logging.info("Warming up GPU...")
-    for _ in range(1):
-        set_random_seed(seed)
-        _ = baseline_pipe(prompt, num_inference_steps=50, output_type='pt').images
-
-    # Baseline
-    logging.info("Running baseline...")
-    start_time = time.time()
-    set_random_seed(seed)
-
-    ori_output = baseline_pipe(prompt, num_inference_steps=50, output_type='pt').images
-    baseline_use_time = time.time() - start_time
-    logging.info("Baseline: {:.2f} seconds".format(baseline_use_time))
-
-    del baseline_pipe
-    torch.cuda.empty_cache()
-
-    # # Zeus
-    # pipe = StableDiffusionXLPipeline.from_pretrained(
+    # baseline_pipe = StableDiffusionXLPipeline.from_pretrained(
     #     args.model, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
     # ).to("cuda:0")
     # if args.solver == "dpm":
-    #     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    #     baseline_pipe.scheduler = DPMSolverMultistepScheduler.from_config(baseline_pipe.scheduler.config)
     # if args.solver == "euler":
-    #     pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
-    # # pipe.load_lora_weights(lora_path)
-
-    # patch.apply_patch(pipe,
-    #                   acc_range=(10, 45),
-
-    #                   interp_mode="psi",
-    #                   caching_mode="reuse_interp",
-    #                   denominator=3,
-    #                   modular=(0, 1, ),
-
-    #                   lagrange_int=4,
-    #                   lagrange_step=24,
-    #                   lagrange_term=4)
+    #     baseline_pipe.scheduler = EulerDiscreteScheduler.from_config(baseline_pipe.scheduler.config)
+    # # baseline_pipe.load_lora_weights(lora_path)
 
     # # Warmup GPU. Only for testing the speed.
     # logging.info("Warming up GPU...")
     # for _ in range(1):
     #     set_random_seed(seed)
-    #     _ = pipe(prompt, num_inference_steps=50, output_type='pt').images
-    #     patch.reset_cache(pipe)
+    #     _ = baseline_pipe(prompt, num_inference_steps=50, output_type='pt').images
 
-    # logging.info("Running ⚡Zeus...")
-    # set_random_seed(seed)
+    # # Baseline
+    # logging.info("Running baseline...")
     # start_time = time.time()
+    # set_random_seed(seed)
 
-    # cap_output = pipe(prompt, num_inference_step=50, output_type='pt').images
-    # use_time = time.time() - start_time
-    # logging.info("⚡Zeus: {:.2f} seconds".format(use_time))
+    # ori_output = baseline_pipe(prompt, num_inference_steps=50, output_type='pt').images
+    # baseline_use_time = time.time() - start_time
+    # logging.info("Baseline: {:.2f} seconds".format(baseline_use_time))
 
-    # logging.info("Baseline: {:.2f} seconds. CAP: {:.2f} seconds".format(baseline_use_time, use_time))
-    save_image(ori_output[0], "/kaggle/working/output.png")
+    # del baseline_pipe
+    # torch.cuda.empty_cache()
+
+    # Zeus
+    pipe = StableDiffusionXLPipeline.from_pretrained(
+        args.model, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    ).to("cuda:0")
+    if args.solver == "dpm":
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+    if args.solver == "euler":
+        pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
+    # pipe.load_lora_weights(lora_path)
+
+    patch.apply_patch(pipe,
+                      acc_range=(10, 45),
+
+                      interp_mode="psi",
+                      caching_mode="reuse_interp",
+                      denominator=3,
+                      modular=(0, 1, ),
+
+                      lagrange_int=4,
+                      lagrange_step=24,
+                      lagrange_term=4)
+
+    # Warmup GPU. Only for testing the speed.
+    logging.info("Warming up GPU...")
+    for _ in range(1):
+        set_random_seed(seed)
+        _ = pipe(prompt, num_inference_steps=50, output_type='pt').images
+        patch.reset_cache(pipe)
+
+    logging.info("Running ⚡Zeus...")
+    set_random_seed(seed)
+    start_time = time.time()
+
+    cap_output = pipe(prompt, num_inference_step=50, output_type='pt').images
+    use_time = time.time() - start_time
+    logging.info("⚡Zeus: {:.2f} seconds".format(use_time))
+
+    logging.info("Baseline: {:.2f} seconds. CAP: {:.2f} seconds".format(baseline_use_time, use_time))
+    save_image(cap_output[0], "/kaggle/working/output.png")
     logging.info("Saved to output.png. Done!")
 
     # print(pipe.unet._cache_bus.rel_momentum_list)
